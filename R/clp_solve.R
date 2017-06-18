afdc736226a3f5f4509a7e0702c998aa56d902dd
Yixuan Qiu <yixuanq@gmail.com>
2017-06-17 20:44:30 -0400

    add example

--- R/clp_solve.R
+++ R/clp_solve.R
@@ -11,7 +11,10 @@
 #'            var_lb <= x     <= var_ub
 #' }
 #'
-#' @param obj A vector representing the objective coefficients \eqn{c}.
+#' @param obj Typically a vector representing the objective coefficients \eqn{c}.
+#'            If \code{obj} is a matrix of \eqn{N} columns, then \eqn{N} linear
+#'            programming problems that have the same constraints but different
+#'            objective coefficients will be solved.
 #' @param A   A matrix representing the constraint matrix \eqn{A}. Supported
 #'            matrix types include \code{matrix}, \code{dgTMatrix}, \code{dgCMatrix},
 #'            \code{dgRMatrix}, and other ones that can be coerced to these
@@ -22,6 +25,36 @@
 #' @param var_ub Upper bounds of the objective variables. Infinite values are allowed.
 #' @param max Logical value. \code{TRUE} for maximization and \code{FALSE} for
 #'            minimization.
+#'
+#' @return A list with the following components:
+#'
+#' \describe{
+#'   \item{\code{solution}}{If the input \code{obj} is a vector, then \code{solution}
+#'                          contains the vector of optimal coefficients. If \code{obj}
+#'                          is a matrix, then the solutions are combined horizontally.}
+#'   \item{\code{objval}}{The value of the objective function at the solution.}
+#'   \item{\code{status}}{Status code for each problem solved.}
+#' }
+#'
+#' @author Yixuan Qiu \email{yixuanq@@gmail.com}
+#'
+#' @export
+#' @keywords optimize
+#' @examples
+#' ## Example from the Rglpk pakcage
+#' ##
+#' ## maximize:   2 x1 + 4 x2 + 3 x3
+#' ## subject to: 3 x1 + 4 x2 + 2 x3 <= 60
+#' ##             2 x1 +   x2 + 2 x3 <= 40
+#' ##               x1 + 3 x2 + 2 x3 <= 80
+#' ## x1, x2, x3 are non-negative real numbers
+#'
+#' obj = c(2, 4, 3)
+#' mat = matrix(c(3, 2, 1, 4, 1, 3, 2, 2, 2), nrow = 3)
+#' constr_ub = c(60, 40, 80)
+#' var_lb = c(0, 0, 0)
+#'
+#' clp_solve(obj, mat, constr_ub = constr_ub, var_lb = var_lb, max = TRUE)
 clp_solve = function(obj, A,
                      constr_lb = rep(-Inf, nrow(A)), constr_ub = rep(Inf, nrow(A)),
                      var_lb = rep(0, ncol(A)), var_ub = rep(Inf, ncol(A)),

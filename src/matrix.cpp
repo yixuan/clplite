245041f02e8202519d81200c98ce8bb3668f493f
Yixuan Qiu <yixuanq@gmail.com>
2017-08-01 01:16:50 -0400

    convert Clp matrix to R matrix

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -2,6 +2,7 @@
 
 using Rcpp::NumericVector;
 using Rcpp::IntegerVector;
+using Rcpp::List;
 
 CoinPackedMatrix* from_dgTMatrix(Rcpp::S4 mat)
 {
@@ -44,4 +45,26 @@ CoinPackedMatrix* from_dgRMatrix(Rcpp::S4 mat)
     cmat->setDimensions(dim[0], dim[1]);
 
     return cmat;
+}
+
+Rcpp::S4 from_CoinPackedMatrix(const CoinPackedMatrix* mat)
+{
+    const bool col_major = mat->isColOrdered();
+    std::string klass = col_major ? "dgCMatrix" : "dgRMatrix";
+    const int nr           = mat->getNumRows();
+    const int nc           = mat->getNumCols();
+    const int nnz          = mat->getNumElements();
+    const double* mat_data = mat->getElements();
+    const int* i_or_j      = mat->getIndices();
+    const int* p           = mat->getVectorStarts();
+
+    Rcpp::S4 res(klass);
+    res.slot(col_major ? "i" : "j") = IntegerVector(i_or_j, i_or_j + nnz);
+    res.slot("p")        = IntegerVector(p, p + mat->getSizeVectorStarts());
+    res.slot("Dim")      = IntegerVector::create(nr, nc);
+    res.slot("Dimnames") = List::create(R_NilValue, R_NilValue);
+    res.slot("x")        = NumericVector(mat_data, mat_data + nnz);
+    res.slot("factors")  = List();
+
+    return res;
 }
--- src/matrix.h
+++ src/matrix.h
@@ -8,5 +8,7 @@ CoinPackedMatrix* from_dgTMatrix(Rcpp::S4 mat);
 CoinPackedMatrix* from_dgCMatrix(Rcpp::S4 mat);
 CoinPackedMatrix* from_dgRMatrix(Rcpp::S4 mat);
 
+Rcpp::S4 from_CoinPackedMatrix(const CoinPackedMatrix* mat);
+
 
 #endif // CLPLITE_MATRIX_H
